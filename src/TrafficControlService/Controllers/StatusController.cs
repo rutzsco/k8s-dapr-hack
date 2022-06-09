02897171386c944a27edd9315018ebbcbce54ad3
Scott Rutz <scrutz@microsoft.com>
2022-06-09 14:29:47 -0500

    Create StatusController.cs

---
+++ src/TrafficControlService/Controllers/StatusController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TrafficControlService.Controllers
+{
+    [ApiController]
+    public class StatusController : Controller
+    {
+        [HttpGet("status")]
+        public IActionResult Get()
+        {
+            return new OkObjectResult("OK");
+        }
+    }
+}
