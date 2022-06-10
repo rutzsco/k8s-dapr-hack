7aa0a61fdce8b34c5759687d433e1b1238ee9519
Scott Rutz <scrutz@microsoft.com>
2022-06-10 18:23:48 -0500

    Update Program.cs

--- src/TrafficControlService/Program.cs
+++ src/TrafficControlService/Program.cs
@@ -15,6 +15,7 @@ namespace TrafficControlService
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder
+                    .UseUrls("http://localhost:6000")
                         .UseStartup<Startup>();
                 });
     }
