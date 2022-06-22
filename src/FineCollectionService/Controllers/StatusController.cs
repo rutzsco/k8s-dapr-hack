291bbd41bcbf4e7ce8eeb2cb66487bbebb358c9f
Scott Rutz <scrutz@microsoft.com>
2022-06-22 15:09:26 -0500

    cp

---
+++ src/FineCollectionService/Controllers/StatusController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace FineCollectionService.Controllers
+{
+    [ApiController]
+    public class StatusController : Controller
+    {
+        private readonly ILogger<StatusController> _logger;
+
+        public StatusController(ILogger<StatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("status")]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Processing reqest - status");
+            return new OkObjectResult("OK-V1");
+        }
+    }
+}
